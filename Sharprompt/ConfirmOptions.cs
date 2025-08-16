e4b1c6127afb1f0742aab5b009748a91daf68a61
akifisitan <102677971+akifisitan@users.noreply.github.com>
2025-08-16 13:26:17 +0300

    add sharprompt

---
+++ Sharprompt/ConfirmOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sharprompt;
+
+public class ConfirmOptions
+{
+    public string Message { get; set; } = null!;
+
+    public bool? DefaultValue { get; set; }
+
+    internal void EnsureOptions()
+    {
+        ArgumentNullException.ThrowIfNull(Message);
+    }
+}
---
+++ Sharprompt/Symbol.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sharprompt;
+
+public class Symbol(string value, string fallbackValue)
+{
+    public override string ToString() => IsUnicodeSupported ? value : fallbackValue;
+
+    public static implicit operator string(Symbol symbol) => symbol.ToString();
+
+    private static bool IsUnicodeSupported => !RuntimeInformation.IsOSPlatform(OSPlatform.Windows) || Console.OutputEncoding.CodePage is 1200 or 65001;
+}
